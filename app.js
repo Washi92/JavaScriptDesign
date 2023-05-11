ca5b929c6563f4e73f7fd6ce368e3970f31ac193
Washi92 <s.d.c@live.it>
2023-05-11 14:22:41 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,59 @@
+
+
+const track = document.getElementById("image-track")
+window.onmousedown = e =>{
+    track.dataset.mouseDownAt = e.clientX;
+}
+
+window.onmousemove = e =>{
+    if(track.dataset.mouseDownAt === "0") return;
+
+    const mouseDelta = parseFloat(track.dataset.mouseDownAt) - e.clientX
+    const maxDelta = window.innerWidth / 2;
+
+    const percentage = (mouseDelta/maxDelta) * -100;
+    const nextPercentageUnconstrained = parseFloat(track.dataset.prevPercentage) + percentage
+    const nextPercentage = Math.max(Math.min(nextPercentageUnconstrained, 0), -100); 
+   
+
+    track.dataset.percentage = nextPercentage;
+
+    track.animate({
+        transform: `translate(${nextPercentage}%, -50%)`
+    }, { duration: 1200, fill: "forwards"})
+  
+
+    for (const image of track.getElementsByClassName("image")){
+        image.animate({
+            objectPosition: `${nextPercentage + 100}% center`
+        }, {duration:1200, fill: "forwards"})
+    }
+}
+
+window.onmouseup= () =>{
+    track.dataset.mouseDownAt = "0";
+    track.dataset.prevPercentage = track.dataset.percentage;    
+}
+
+const letters = "QWERTYUIOPASDFGHJKLZXCVBNM1234567890"
+let interval = null;
+
+document.querySelector("h1").onmouseover = event =>{
+    let iterations = 0;
+    clearInterval(interval);
+
+    interval = setInterval(()=>{
+        event.target.innerText = event.target.innerText
+        .split("")
+        .map((letter, index) => {
+            if(index < iterations){
+                return event.target.dataset.value[index];
+            }
+            return letters[Math.floor(Math.random() * 26)];
+
+        }).join("");
+
+    if(iterations >= event.target.dataset.value.length) clearInterval(interval);
+    iterations += 1/3;
+    }, 30)
+}
---
+++ readme.md
@@ -0,0 +1,2 @@
+This project use simples concepts to scroll images in a fancy way, and change the text with a cool effect.
+The aim is to save and keep track of the knowledge and use it in other projects.
